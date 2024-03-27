c288c9a55185eb0bac1a21c44c4cc174545907a5
SimHorner <77296365+SimHorner@users.noreply.github.com>
2024-03-27 08:14:20 +0000

    sim

---
+++ VeryGreedy Allocation output.rb
@@ -0,0 +1,192 @@
+Assigned product 34 -> shelf 0
+There are 96 shelves yet to be assigned
+Assigned product 31 -> shelf 24
+There are 95 shelves yet to be assigned
+Assigned product 35 -> shelf 1
+There are 94 shelves yet to be assigned
+Assigned product 71 -> shelf 25
+There are 93 shelves yet to be assigned
+Assigned product 70 -> shelf 2
+There are 92 shelves yet to be assigned
+Assigned product 15 -> shelf 26
+There are 91 shelves yet to be assigned
+Assigned product 18 -> shelf 3
+There are 90 shelves yet to be assigned
+Assigned product 30 -> shelf 27
+There are 89 shelves yet to be assigned
+Assigned product 2 -> shelf 4
+There are 88 shelves yet to be assigned
+Assigned product 51 -> shelf 28
+There are 87 shelves yet to be assigned
+Assigned product 76 -> shelf 5
+There are 86 shelves yet to be assigned
+Assigned product 58 -> shelf 29
+There are 85 shelves yet to be assigned
+Assigned product 67 -> shelf 6
+There are 84 shelves yet to be assigned
+Assigned product 6 -> shelf 30
+There are 83 shelves yet to be assigned
+Assigned product 49 -> shelf 7
+There are 82 shelves yet to be assigned
+Assigned product 50 -> shelf 31
+There are 81 shelves yet to be assigned
+Assigned product 4 -> shelf 48
+There are 80 shelves yet to be assigned
+Assigned product 11 -> shelf 72
+There are 79 shelves yet to be assigned
+Assigned product 83 -> shelf 8
+There are 78 shelves yet to be assigned
+Assigned product 28 -> shelf 32
+There are 77 shelves yet to be assigned
+Assigned product 10 -> shelf 49
+There are 76 shelves yet to be assigned
+Assigned product 38 -> shelf 73
+There are 75 shelves yet to be assigned
+Assigned product 33 -> shelf 50
+There are 74 shelves yet to be assigned
+Assigned product 48 -> shelf 74
+There are 73 shelves yet to be assigned
+Assigned product 52 -> shelf 9
+There are 72 shelves yet to be assigned
+Assigned product 60 -> shelf 33
+There are 71 shelves yet to be assigned
+Assigned product 78 -> shelf 10
+There are 70 shelves yet to be assigned
+Assigned product 43 -> shelf 34
+There are 69 shelves yet to be assigned
+Assigned product 8 -> shelf 51
+There are 68 shelves yet to be assigned
+Assigned product 55 -> shelf 11
+There are 67 shelves yet to be assigned
+Assigned product 63 -> shelf 35
+There are 66 shelves yet to be assigned
+Assigned product 1 -> shelf 75
+There are 65 shelves yet to be assigned
+Assigned product 19 -> shelf 12
+There are 64 shelves yet to be assigned
+Assigned product 47 -> shelf 36
+There are 63 shelves yet to be assigned
+Assigned product 44 -> shelf 13
+There are 62 shelves yet to be assigned
+Assigned product 13 -> shelf 52
+There are 61 shelves yet to be assigned
+Assigned product 39 -> shelf 76
+There are 60 shelves yet to be assigned
+Assigned product 59 -> shelf 37
+There are 59 shelves yet to be assigned
+Assigned product 66 -> shelf 53
+There are 58 shelves yet to be assigned
+Assigned product 57 -> shelf 77
+There are 57 shelves yet to be assigned
+Assigned product 72 -> shelf 14
+There are 56 shelves yet to be assigned
+Assigned product 46 -> shelf 38
+There are 55 shelves yet to be assigned
+Assigned product 84 -> shelf 54
+There are 54 shelves yet to be assigned
+Assigned product 74 -> shelf 78
+There are 53 shelves yet to be assigned
+Assigned product 37 -> shelf 55
+There are 52 shelves yet to be assigned
+Assigned product 32 -> shelf 79
+There are 51 shelves yet to be assigned
+Assigned product 65 -> shelf 15
+There are 50 shelves yet to be assigned
+Assigned product 20 -> shelf 39
+There are 49 shelves yet to be assigned
+Assigned product 64 -> shelf 56
+There are 48 shelves yet to be assigned
+Assigned product 69 -> shelf 80
+There are 47 shelves yet to be assigned
+Assigned product 68 -> shelf 16
+There are 46 shelves yet to be assigned
+Assigned product 79 -> shelf 57
+There are 45 shelves yet to be assigned
+Assigned product 89 -> shelf 40
+There are 44 shelves yet to be assigned
+Assigned product 16 -> shelf 81
+There are 43 shelves yet to be assigned
+Assigned product 77 -> shelf 17
+There are 42 shelves yet to be assigned
+Assigned product 82 -> shelf 58
+There are 41 shelves yet to be assigned
+Assigned product 29 -> shelf 82
+There are 40 shelves yet to be assigned
+Assigned product 90 -> shelf 59
+There are 39 shelves yet to be assigned
+Assigned product 21 -> shelf 83
+There are 38 shelves yet to be assigned
+Assigned product 45 -> shelf 41
+There are 37 shelves yet to be assigned
+Assigned product 87 -> shelf 18
+There are 36 shelves yet to be assigned
+Assigned product 22 -> shelf 42
+There are 35 shelves yet to be assigned
+Assigned product 88 -> shelf 19
+There are 34 shelves yet to be assigned
+Assigned product 27 -> shelf 43
+There are 33 shelves yet to be assigned
+Assigned product 24 -> shelf 60
+There are 32 shelves yet to be assigned
+Assigned product 5 -> shelf 84
+There are 31 shelves yet to be assigned
+Assigned product 14 -> shelf 61
+There are 30 shelves yet to be assigned
+Assigned product 54 -> shelf 85
+There are 29 shelves yet to be assigned
+Assigned product 36 -> shelf 62
+There are 28 shelves yet to be assigned
+Assigned product 23 -> shelf 86
+There are 27 shelves yet to be assigned
+Assigned product 80 -> shelf 20
+There are 26 shelves yet to be assigned
+Assigned product 56 -> shelf 63
+There are 25 shelves yet to be assigned
+Assigned product 9 -> shelf 44
+There are 24 shelves yet to be assigned
+Assigned product 75 -> shelf 87
+There are 23 shelves yet to be assigned
+Assigned product 53 -> shelf 21
+There are 22 shelves yet to be assigned
+Assigned product 61 -> shelf 45
+There are 21 shelves yet to be assigned
+Assigned product 12 -> shelf 64
+There are 20 shelves yet to be assigned
+Assigned product 25 -> shelf 88
+There are 19 shelves yet to be assigned
+Assigned product 26 -> shelf 22
+There are 18 shelves yet to be assigned
+Assigned product 40 -> shelf 65
+There are 17 shelves yet to be assigned
+Assigned product 62 -> shelf 46
+There are 16 shelves yet to be assigned
+Assigned product 41 -> shelf 89
+There are 15 shelves yet to be assigned
+Assigned product 3 -> shelf 66
+There are 14 shelves yet to be assigned
+Assigned product 86 -> shelf 23
+There are 13 shelves yet to be assigned
+Assigned product 73 -> shelf 90
+There are 12 shelves yet to be assigned
+Assigned product 85 -> shelf 67
+There are 11 shelves yet to be assigned
+Assigned product 7 -> shelf 91
+There are 10 shelves yet to be assigned
+Assigned product 42 -> shelf 68
+There are 9 shelves yet to be assigned
+Assigned product 81 -> shelf 47
+There are 8 shelves yet to be assigned
+Assigned product 17 -> shelf 69
+There are 7 shelves yet to be assigned
+Assigned product 4 -> shelf 92
+There are 6 shelves yet to be assigned
+Assigned product 33 -> shelf 70
+There are 5 shelves yet to be assigned
+Assigned product 8 -> shelf 71
+There are 4 shelves yet to be assigned
+Assigned product 39 -> shelf 93
+There are 3 shelves yet to be assigned
+Assigned product 1 -> shelf 94
+There are 2 shelves yet to be assigned
+Assigned product 11 -> shelf 95
+There are 1 shelves yet to be assigned
